137a4a1f9c4e9d1825dc94d8668bd1b945289e1c
52525f <adavis100@gmail.com>
2017-02-19 11:15:50 -0800

    Add zsh plugin download

--- init.sh
+++ init.sh
@@ -10,5 +10,8 @@ then
   mv ~/.zshrc ~/.zshrc.bak
 fi
 
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/zshrc ~/.zshrc
