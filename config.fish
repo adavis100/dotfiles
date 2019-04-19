a94105257d681a46b389638cac24604373736673
Aaron Davis <adavis100@gmail.com>
2019-04-19 14:31:18 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,25 +7,46 @@ function get
   git $argv
 end
 
-function ct
-  pushd ~/mf/core-server/dev/test/assemblies/container/target/centralmgmt-test-container-master-SNAPSHOT
+function ls
+  /bin/ls -FGh $argv
 end
 
-function core
-  pushd ~/mf/core-server/dev/test/assemblies/container/target/centralmgmt-test-container-master-SNAPSHOT/bin
-  ./server console
+alias hr 'history --merge'  # read and merge history from disk
+
+# Use ISO dates and thousands separator in GNU coreutils (e.g., ls)
+set -x TIME_STYLE long-iso
+set -x BLOCK_SIZE "'1"
+
+if not set -q abbrs_initialized
+  set -U abbrs_initialized
+  echo -n Setting abbreviations...
+
+  abbr g 'git'
+  abbr ga 'git add'
+  abbr gb 'git branch'
+  abbr gbl 'git blame'
+  abbr gc 'git commit -m'
+  abbr gco 'git checkout'
+  abbr gcp 'git cherry-pick'
+  abbr gd 'git diff'
+  abbr gf 'git fetch'
+  abbr gl 'git log'
+  abbr gls 'git ls'
+  abbr gll 'git ll'
+  abbr gm 'git merge'
+  abbr gp 'git pull'
+  abbr gpr 'git pull --rebase'
+  abbr gpu 'git push'
+  abbr gr 'git remote'
+  abbr gs 'git status'
+  abbr gst 'git stash'
+
+  abbr gbr 'git branch | grep -v master | xargs git branch -D'
+
+  echo 'Done'
 end
 
-function cb
-  pushd ~/mf/core-server/dev
-  mvn clean
-  mvn clean -DbuildContainers -PfuncTests,testContainer
-  mvn install -DskipTests
-  if test $status -eq 0
-    mvn install -DbuildContainers -PtestContainer,funcTests -DskipTests
-  end
-end
-
+# move to top of repository
 function rt
   cd (git rev-parse --show-cdup)
 end
